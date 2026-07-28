ceba6d4f5616d0354591ff2e18c0adce5844cece
josemariacamposb32-cmd <josemariacamposb32@gmail.com>
2026-07-28 23:15:12 +0200

    Add Supabase configuration file

---
+++ supabase-config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://utvlhbasyuvodrwsptbj.supabase.co";
+
+const SUPABASE_ANON_KEY = "sb_publishable_iXQsgIIZ_XpfJHZW_eUaVA_cSpmnaKL";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
